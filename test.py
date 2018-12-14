440634427befce31903a0aab59c2f999f82575d6
neo <neo.hung@quantatw.com>
2018-12-14 17:00:41 +0800

    test

--- test.py
+++ test.py
@@ -1,2 +1,25 @@
+# -*- coding: utf-8 -*-
 import os
+import json
+import gevent
+from flask import Flask, render_template, request, send_from_directory
+from flask_sockets import Sockets
+import datetime
+import time
+import base64
+import numpy
+import redis
+import uuid
+
+from linebot import LineBotApi
+from linebot.models import TextSendMessage, ImageSendMessage
+from linebot.exceptions import LineBotApiError
+
 print("TEST")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+    #from gevent import pywsgi
+    #from geventwebsocket.handler import WebSocketHandler
+    #server = pywsgi.WSGIServer(('', 8080), app, handler_class=WebSocketHandler)
+    #server.serve_forever()

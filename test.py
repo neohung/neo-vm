00d58539bc023af19e3f2b591da9bf1324c25de6
neo <neo.hung@quantatw.com>
2018-12-17 15:04:06 +0800

    test

--- test.py
+++ test.py
@@ -11,6 +11,17 @@ sockets = Sockets(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    #return render_template('index.html')
     return "TEST NEO"
 
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
+
+@sockets.route('/echo')
+def communicator(ws):
+    print("New player connecting!", ws)
+    while not ws.closed:
+        message = ws.receive()
+        print("receiver say: %s" % (message))
+    

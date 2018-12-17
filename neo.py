db83eef7ffd1c0537119a835476bcba2668e7314
neo <neo.hung@quantatw.com>
2018-12-17 14:18:07 +0800

    test

--- neo.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-application = Flask(__name__)
-
-@application.route("/")
-def hello():
-    return "Hello NeO!"
-
-if __name__ == "__main__":
-    application.run()
--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,9 @@
-#
+from flask import Flask
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return "Hello NeO!"
+
+if __name__ == "__main__":
+    application.run()

604cd5b16f5f54d26067d046e32816e56ea21f7c
neo <neo.hung@quantatw.com>
2018-12-17 14:35:36 +0800

    test

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,19 @@
+import os
+import time
+import datetime
 from flask import Flask
+
+REDIS_URL="redis://127.0.0.1:6379/0"
+if 'REDIS_URL' in os.environ:
+  REDIS_URL = os.environ['REDIS_URL']
+
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello NeO!"
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return now+": "+REDIS_URL
 
 if __name__ == "__main__":
     application.run()
